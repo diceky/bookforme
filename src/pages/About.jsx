4f4ab96ae3d7ed4d8cd3de86ad0328c48151d205
diceky <daisuke4yukita@gmail.com>
2024-07-29 18:19:58 +0900

    update About page text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,8 +9,8 @@ const About = () => {
         <p className={Styles.text}>At <a href="https://duhhh.co" className={Styles.link} target="_blank" rel="noreferrer">duhhh</a> we're thinking deeply about practical values GenAI can bring to us, beyond assisstants, knowledge bases and fancy multimodality.</p>
         <p className={Styles.text}>One interesting insight we found recently was how GenAI can relieve psychological stress in our daily lives, by having it do tasks that makes us humans think twice.</p>
         <p className={Styles.text}>Making phone calls to restaurants is a perfect example. Especially in cases when you don't speak the local language.</p>
-        <p className={Styles.text}>This may be the future. This may not be. Who knows.</p>
-        <p className={Styles.text}>Are YOU ready to have an AI make a first reservation call?</p>
+        <p className={Styles.text}>This may be the future. This may not. Who knows.</p>
+        <p className={Styles.text}>But at least it seems to be solving an actual problem 🤷‍♀️.</p>
     </div>
   )
 }
