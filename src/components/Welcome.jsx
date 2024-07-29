cc79c4d290d1924812cf00f6b5d1927894888788
diceky <daisuke4yukita@gmail.com>
2024-07-29 18:15:14 +0900

    add GIF to About page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -12,9 +12,7 @@ const Welcome = ({onButton}) => {
             Many restaurants still require us to call them to make a reservation. <br />
             And that sucks. Let's face it, who likes making phone calls? <br /><br />
             <span className={Styles.highlight}>But wait... we live in an age of AI, right??</span><br /><br />
-            So we thought, why not have AI make that call for us? In any language of our choice?<br/>
-            This may be the future. This may not be. Who knows.<br/>
-            Are YOU ready to have an AI make a first reservation call?<br /><br />
+            So we thought, why not have AI make that call for us? In any language of our choice?<br/><br />
             <span className={Styles.warning}>(We're not joking, it will make a real phone call. So don't play with real numbers.)</span>
         </p>
         <button className={Styles.button} onClick={onButton}>Let's go</button>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,12 @@ const About = () => {
   return (
     <div className={Styles.wrapper}>
         <p className={Styles.title}>What can GenAI do for us? <br/>For real?</p>
+        <iframe className={Styles.gif} src="https://giphy.com/embed/l0MYM5ZAXGvK1KSPK"></iframe>
         <p className={Styles.text}>At <a href="https://duhhh.co" className={Styles.link} target="_blank" rel="noreferrer">duhhh</a> we're thinking deeply about practical values GenAI can bring to us, beyond assisstants, knowledge bases and fancy multimodality.</p>
         <p className={Styles.text}>One interesting insight we found recently was how GenAI can relieve psychological stress in our daily lives, by having it do tasks that makes us humans think twice.</p>
         <p className={Styles.text}>Making phone calls to restaurants is a perfect example. Especially in cases when you don't speak the local language.</p>
+        <p className={Styles.text}>This may be the future. This may not be. Who knows.</p>
+        <p className={Styles.text}>Are YOU ready to have an AI make a first reservation call?</p>
     </div>
   )
 }
